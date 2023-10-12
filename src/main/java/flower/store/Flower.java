b901c426313ac809e609c9511181134bf5e230a3
dyshleva <116552489+dyshleva@users.noreply.github.com>
2023-10-12 10:21:07 +0300

    1 commit

--- src/main/java/flower/store/Flower.java
+++ src/main/java/flower/store/Flower.java
@@ -1,21 +1,29 @@
 package flower.store;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
-
-
+import lombok.NoArgsConstructor;
 
 @Setter
-public class Flower {
-    @Getter
-    private double sepalLength;
-    private FlowerColor color;
-    @Getter
-    private double price;
-    @Getter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Flower extends Item {
     private FlowerType flowerType;
+    private FlowerColor color;
+    private double price;
+    private double sepalLength;
+
+    public Flower(Flower flower) {
+        this.color = flower.color;
+        this.price = flower.price;
+        this.sepalLength = flower.sepalLength;
+        this.flowerType = flower.flowerType;
+    }
 
     public String getColor() {
         return color.toString();
     }
+
 }
--- src/main/java/flower/store/FlowerColor.java
+++ src/main/java/flower/store/FlowerColor.java
@@ -1,10 +1,8 @@
 package flower.store;
 
 public enum FlowerColor {
-    /**
-     * Flower colors.
-     */
     RED("#FF0000"), BLUE("#0000FF");
+
     private final String stringRepresentation;
 
     FlowerColor(String stringRepresentation) {
